804d02bd44fada2b39dfd6c2c5ee0e50abe5a707
Cona <codingnuna@gmail.com>
2020-01-16 09:53:03 +0900

    Quiz game

--- game.js
+++ game.js
@@ -1,5 +1,11 @@
 const guestion = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
+const progressText = document.getElementById("progressText");
+const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull");
+
+const loader = document.getElementById("loader");
+const game = document.getElementById("game");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -7,32 +13,53 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-    {
-        question: "Animal?",
-        choice1: "Dog",
-        choice2: "Cat",
-        choice3: "Elephant",
-        choice4: "Horse",
-        answer: 1
-    },
-    {
-        question: "Car?",
-        choice1: "Truck",
-        choice2: "Taxi",
-        choice3: "Bus",
-        choice4: "Fire Engine",
-        answer: 3
-    },
-    {
-        question: "Fruit?",
-        choice1: "Apple",
-        choice2: "Orange",
-        choice3: "Banana",
-        choice4: "Kiwi",
-        answer: 4
-    }
-];
+let questions = [];
+
+// fetch("questions.json")
+//   .then(res => {
+//       return res.json();
+//   })
+//   .then(loadedQuestions => {
+//       console.log(loadedQuestions);
+//       questions = loadedQuestions;
+//       startGame();
+//   })
+//   .catch(err => {
+//       console.error(err);
+//   })
+
+fetch(
+    "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple"
+  )
+    .then(res => {
+      return res.json();
+    })
+    .then(loadedQuestions => {
+      console.log(loadedQuestions.results);
+      questions = loadedQuestions.results.map(loadedQuestion => {
+        const formattedQuestion = {
+          question: loadedQuestion.question
+        };
+  
+        const answerChoices = [...loadedQuestion.incorrect_answers];
+        formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
+        answerChoices.splice(
+          formattedQuestion.answer - 1,
+          0,
+          loadedQuestion.correct_answer
+        );
+  
+        answerChoices.forEach((choice, index) => {
+          formattedQuestion["choice" + (index + 1)] = choice;
+        });
+  
+        return formattedQuestion;
+      });
+      startGame();
+    })
+    .catch(err => {
+      console.error(err);
+    });
 
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 3;
@@ -41,15 +68,21 @@ startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
-    console.log(availableQuestions);
     getNewQuestions();
+
+    game.classList.remove("hidden");
+    loader.classList.add("hidden");
 }
 
 getNewQuestions = () => {
     if (availableQuestions.length == 0 || questionCounter >= MAX_QUESTIONS) {
+        localStorage.setItem("mostRecentScore", score);
         return window.location.assign("/end.html");
     }
     questionCounter++;
+    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
@@ -60,7 +93,6 @@ getNewQuestions = () => {
     });
 
     availableQuestions.splice(questionIndex, 1);
-    console.log(availableQuestions);
     acceptingAnswers = true;
 }
 
@@ -71,9 +103,24 @@ choices.forEach(choice => {
         acceptingAnswers = false;
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset["number"];
-        console.log(selectedAnswer);
-        getNewQuestions();
+
+        const classToApply = 
+        selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+        if (classToApply === "correct") {
+            incrementScore(CORRECT_BONUS);
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply);
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestions();
+        }, 1000);        
     });
 });
 
-startGame();
+incrementScore = num => {
+    score += num;
+    scoreText.innerText = score;
+}
